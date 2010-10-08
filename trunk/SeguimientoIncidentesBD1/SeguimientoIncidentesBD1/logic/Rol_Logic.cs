8d02f1a65bb59c6466b1df009ae94aefe42c427f
tacapc <tacapc@e89fb46a-8075-351f-5211-a5414b4f9151>
2010-10-08 03:41:16 +0000

    Puse las clases básicas e hice la parte de logica del rol y parte de la persistencia.
    Cosas a tener en cuenta:
    a. Tratemos de manejar siempre la misma nomenclatura de variables y que estas se condigan
    con los nombres de los campos en la BD.
    b. Manejemos el tema de excepciones ya que despues va a ser más facil del debugging.
    c. Usemos los sqlcommands como se muestra en la clase Rol_Persist.

---
+++ trunk/SeguimientoIncidentesBD1/SeguimientoIncidentesBD1/logic/Rol_Logic.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SeguimientoIncidentesBD1.persist;
+
+namespace SeguimientoIncidentesBD1.logic
+{
+    public class Rol_Logic
+    {
+        private string rolCod;
+        private string rolDesc;
+        //un rol tiene una lista de seguridades, cada seguridad se corresponde con un nombre programa de la interfaz
+        private int[] rolSegId;
+
+        public string RolCod
+        {
+            get { return rolCod; }
+            set { rolCod = value; }
+        }
+
+        public string RolDesc
+        {
+            get { return rolDesc; }
+            set { rolDesc = value; }
+        }
+        
+        public int[] RolSegId
+        {
+            get { return rolSegId; }
+            set { rolSegId = value; }
+        }
+
+        public Rol_Logic(string rolCod, string rolDesc, int[]rolSegId){
+            this.rolCod = rolCod;
+            this.rolDesc = rolDesc;
+            this.rolSegId = rolSegId;
+        }
+
+        public Rol_Logic(string rolCod, int[]rolSegId){
+            this.rolCod = rolCod;
+            this.rolSegId = rolSegId;
+        }
+
+        //cada entidad debe tener un constructor que solo reciba la clave, y en dicho caso haga la busqueda en la BD
+        //y cargue los datos a partir de los datos del rol en la base
+        public Rol_Logic(string rolCod)
+        {
+            try
+            {
+                Rol_Persist rolPersist = new Rol_Persist(rolCod);
+                this.rolCod = rolPersist.RolCod;
+                this.rolDesc = rolPersist.RolDesc;
+                this.RolSegId = rolPersist.RolSegId;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        //persiste un rol en la base de datos
+        public void RolPersist(){
+            try
+            {
+                Rol_Persist rolPersist = new Rol_Persist(this.rolCod, this.rolDesc, this.rolSegId);
+                rolPersist.RolCreate();
+            }
+            catch (Exception ex)
+            {
+                //el metodo CrearRol de la clase Rol_Persist debe hacer un throw de la excepcion en caso de que no
+                //se pueda persistir el rol en la base de datos.
+                //ESTE MANEJO DE EXPCECIONES DEBE REALIZARSE SIEMPRE
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        //persiste un rol en la base de datos
+        public void RolDelete(string rolCod)
+        {
+            try
+            {
+                Rol_Persist rolPersist = new Rol_Persist(rolCod);
+                rolPersist.RolDelete();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        //persiste un rol en la base de datos
+        public void RolModify(string rolCod, string nuevaDesc, int[] rolSegId)
+        {
+            try
+            {
+                Rol_Persist rolPersist = new Rol_Persist(rolCod);
+                //primero actualizo la nueva descripción
+                rolPersist.RolDescUpdate(nuevaDesc);
+                //luego comparo uno a uno los rolSegId, si no existe, lo agrego
+                foreach (int rolSeg in rolSegId)
+                {
+                    Boolean rolExiste = false;
+                    foreach (int rolSegExistente in rolPersist.RolSegId)
+                    {
+                        if (rolSeg == rolSegExistente)
+                        {
+                            rolExiste = true;
+                            break;
+                        }
+                    }
+                    if (!rolExiste)
+                    {
+                        rolPersist.RolSegIdAdd(rolSeg);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ trunk/SeguimientoIncidentesBD1/SeguimientoIncidentesBD1/persist/Rol_Persist.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SeguimientoIncidentesBD1.persist
+{
+    class Rol_Persist
+    {
+        private string rolCod;
+        private string rolDesc;
+        private int[] rolSegId;
+
+        public string RolCod
+        {
+            get { return rolCod; }
+            set { rolCod = value; }
+        }
+
+        public string RolDesc
+        {
+            get { return rolDesc; }
+            set { rolDesc = value; }
+        }
+
+        public int[] RolSegId
+        {
+            get { return rolSegId; }
+            set { rolSegId = value; }
+        }
+
+        public Rol_Persist(string rolCod, string rolDesc, int[] rolSegId)
+        {
+            this.rolCod = rolCod;
+            this.rolDesc = rolDesc;
+            this.rolSegId = rolSegId;
+        }
+
+        //este al recibir el rolCod debe hacer una busqueda en la BD y traer los datos
+        public Rol_Persist(string rolCod)
+        {
+        }
+
+        public void RolCreate()
+        {
+            SqlCommand sql = new SqlCommand();
+            foreach (int rolSeg in this.rolSegId)
+            {
+                sql.CommandText = "INSERT INTO roles (rolCod, rolDes, rolSegId) VALUES (@rolCod, @rolDes, @rolSegId)";
+                sql.Parameters.AddWithValue("@rolCod", this.rolCod);
+                sql.Parameters.AddWithValue("@rolDes", this.rolDesc);
+                //la base de datos debería controlar que el id exista en la tabla de seguridades
+                sql.Parameters.AddWithValue("@rolSegId", rolSeg);
+                SQLInsert sqlIns = new SQLInsert();
+                sqlIns.Execute(sql);
+            }
+            /*los parametros van marcados con @*/
+
+            /* Con el SQLCommandBuilder se persisten los cambios hechos en un DataSet en la
+             * correspondiente tabla en nuestra base de datos, sin tener que armar el insert, update
+             * o delete manualmente
+             * 
+             * Para su uso hay que usar el objeto GridView
+             */
+        }
+
+        internal void RolDescUpdate(string nuevaDesc)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RolSegIdAdd(int rolSeg)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RolDelete()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RolCreate(string p, string p_2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
